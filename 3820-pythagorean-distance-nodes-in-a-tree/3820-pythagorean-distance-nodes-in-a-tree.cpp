78849b7c8648aa32d7e44b11b438dfcfc0fe98e2
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-06-13 13:39:32 +0530

    Time: 332 ms (79.38%), Space: 378 MB (88.72%) - LeetHub

--- 3820-pythagorean-distance-nodes-in-a-tree/3820-pythagorean-distance-nodes-in-a-tree.cpp
+++ 3820-pythagorean-distance-nodes-in-a-tree/3820-pythagorean-distance-nodes-in-a-tree.cpp
@@ -1,85 +1,43 @@
-// class Solution {
-// public:
-//     bool pytha(int a, int b, int c) {
-//         long long x = a, y = b, z = c;
-//         long long total = x + y + z;
-//         long long mn = min({x, y, z});
-//         long long mx = max({x, y, z});
-//         long long mid = total - mn - mx; 
-//         return mn * mn + mid * mid == mx * mx;
-//     }
-//     void dfs(int node, int parent, int dist,vector<vector<int>>& adj,vector<int>& d) {
-//         d[node] = dist;
-//         for (int nei : adj[node]) {
-//             if (nei != parent) {
-//                 dfs(nei, node, dist + 1, adj, d);
-//             }
-//         }
-//     }
-//     int specialNodes(int n, vector<vector<int>>& edges,int x, int y, int z) {
-//         vector<vector<int>> adj(n);
-//         for (auto &e : edges) {
-//             adj[e[0]].push_back(e[1]);
-//             adj[e[1]].push_back(e[0]);
-//         }
-//         vector<int> distX(n), distY(n), distZ(n);
-//         dfs(x, -1, 0, adj, distX);
-//         dfs(y, -1, 0, adj, distY);
-//         dfs(z, -1, 0, adj, distZ);
-//         int ans = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (pytha(distX[i], distY[i], distZ[i])) {
-//                 ans++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-// BFS
-
 class Solution {
 public:
-    bool pytha(int a, int b, int c) {
-        long long x = a, y = b, z = c;
-        long long total = x + y + z;
-        long long mn = min({x, y, z});
-        long long mx = max({x, y, z});
-        long long mid = total - mn - mx; 
-        
-        return mn * mn + mid * mid == mx * mx;
+    typedef long long ll;
+    bool ispytha(int a,int b,int c){
+        ll mx = max(a,max(b,c));
+        ll mi = min(a,min(b,c));
+        ll mid = a+b+c-mx-mi;
+        return mx*mx == mi*mi + mid*mid;
     }
-    void bfs(int start, int n, vector<vector<int>>& adj, vector<int>& dist) {
+    void bfs(int start,vector<int>& dis,vector<vector<int>>& adj){
         queue<int> q;
         q.push(start);
-        dist[start] = 0; 
-        while (!q.empty()) {
+        dis[start]=0;
+        while(!q.empty()){
             int node = q.front();
             q.pop();
-            for (int nei : adj[node]) {
-                if (dist[nei] == -1) {
-                    dist[nei] = dist[node] + 1;
-                    q.push(nei);
+            for(auto it: adj[node]){
+                if(dis[it]==-1){
+                    dis[it] = dis[node]+1;
+                    q.push(it);
                 }
             }
         }
     }
     int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
         vector<vector<int>> adj(n);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        for(int i =0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<int> distX(n, -1), distY(n, -1), distZ(n, -1);
-        bfs(x, n, adj, distX);
-        bfs(y, n, adj, distY);
-        bfs(z, n, adj, distZ);
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (pytha(distX[i], distY[i], distZ[i])) {
+        vector<int> dx(n,-1),dy(n,-1),dz(n,-1);
+        bfs(x,dx,adj);
+        bfs(y,dy,adj);
+        bfs(z,dz,adj);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(ispytha(dx[i],dy[i],dz[i])){
                 ans++;
             }
         }
-        return ans;
+        return ans; 
     }
 };
