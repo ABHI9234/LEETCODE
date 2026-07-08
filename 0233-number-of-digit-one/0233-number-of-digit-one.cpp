b241be810bf9dc30911aa1ee982a0b249bc48081
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-08 17:37:00 +0530

    Time: 2 ms (5.08%), Space: 8.4 MB (12.05%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int solve(string& s,int idx, int tight, int cnt,vector<vector<vector<int>>>& dp) {
-        if (idx == s.size()) {
-            return cnt;
-        }
-        if (dp[idx][tight][cnt] != -1) {
-            return dp[idx][tight][cnt];
-        }
-        int limit = tight ? (s[idx] - '0') : 9;
+    int solve(string &s, int i,int tight,int cnt,vector<vector<vector<int>>>& dp){
+        if(i==s.length()) return cnt;
+        if(dp[i][tight][cnt]!=-1) return dp[i][tight][cnt];
+        
+        int limit = tight?(s[i]-'0'):9;
         int ans = 0;
-        for (int i = 0; i <= limit; i++) {
-            int nextCnt = cnt + (i == 1 ? 1 : 0);
-            int nextTight = tight && (i == limit);
-            ans += solve(s,idx + 1, nextTight, nextCnt,dp);
+        for(int j =0;j<=limit;j++){
+            int ncnt = cnt+ (j==1 ? 1:0);
+            int ntight = tight && (j==limit);
+            ans+=solve(s,i+1,ntight,ncnt,dp);
         }
-        return dp[idx][tight][cnt] = ans;
+        return dp[i][tight][cnt] = ans;
     }
     int countDigitOne(int n) {
-        if (n <= 0) return 0;
         string s = to_string(n);
         int len = s.size();
-        vector<vector<vector<int>>> dp(len, vector<vector<int>>(2, vector<int>(len + 1, -1)));
-        return solve(s,0, 1, 0,dp);
+        vector<vector<vector<int>>> dp(len,vector<vector<int>>(2,vector<int>(len+1,-1)));
+        return solve(s,0,1,0,dp);
     }
 };
