a3795558f3382be77d5686e7e65b97bd10798f86
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-14 21:32:19 +0530

    Time: 0 ms (100%), Space: 21.2 MB (37.56%) - LeetHub

---
+++ 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums) {
+        int sum = nums[0]+nums[1];
+        int c = 1;
+        int n = nums.size();
+        for(int i =2;i<n-1;i+=2){
+            if(nums[i]+nums[i+1]!=sum) break;
+            c++;
+        }
+        return c;
+    }
+};
