082e17d49721131e9ab047850a7e0ca4de51533b
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-14 17:36:29 +0530

    Time: 40 ms (60.12%), Space: 66.2 MB (47.25%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,68 +1,30 @@
-// class Solution {
-// public:
-//     bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
-//         if (state[node] != 0)
-//             return state[node] == 2;
-//         state[node] = 1;
-//         for (int nei : graph[node]) {
-//             if (!dfs(nei, graph, state))
-//                 return false;
-//         }
-//         state[node] = 2; 
-//         return true;
-//     }
-//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-//         int n = graph.size();
-//         vector<int> state(n, 0);
-//         vector<int> ans;
-//         for (int i = 0; i < n; i++) {
-//             if (dfs(i, graph, state))
-//                 ans.push_back(i);
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        
-        // revGraph will store the reversed edges
-        vector<vector<int>> revGraph(n);
-        vector<int> indegree(n, 0);
-        
-        // 1. Reverse the graph and calculate the new indegrees
-        for (int i = 0; i < n; i++) {
-            for (auto it : graph[i]) {
-                revGraph[it].push_back(i);
-                indegree[i]++; // The outdegree of the original is the indegree of the reversed
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+        for(int i =0;i<n;i++){
+            for(auto it: graph[i]){
+                adj[it].push_back(i);
+                indegree[i]++;
             }
         }
-        
         queue<int> q;
-        vector<int> safeNodes;
-        
-        // 2. Start with nodes that have an indegree of 0 in the reversed graph (Terminal Nodes)
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) q.push(i);
+        for(int i =0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
         }
-        
-        // 3. Process the queue (Kahn's Algorithm)
-        while (!q.empty()) {
+        vector<int> ans;
+        while(!q.empty()){
             int node = q.front();
+            ans.push_back(node);
             q.pop();
-            safeNodes.push_back(node);
-            
-            for (auto x : revGraph[node]) {
-                indegree[x]--;
-                if (indegree[x] == 0) q.push(x);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        
-        // 4. The problem strictly requires the result to be sorted in ascending order
-        sort(safeNodes.begin(), safeNodes.end());
-        
-        return safeNodes;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
