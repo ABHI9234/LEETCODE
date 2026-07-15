8ea3e357ba186d59e4aa7524e3a8aaa10a7754ba
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-15 17:29:00 +0530

    Time: 87 ms (7.57%), Space: 133.1 MB (18.92%) - LeetHub

--- 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.cpp
+++ 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int minRectanglesToCoverPoints(vector<vector<int>>& points, int w) {
-        sort(points.begin(), points.end());
+    int minRectanglesToCoverPoints(vector<vector<int>>& nums, int w) {
         int c = 0;
+        sort(nums.begin(),nums.end());
         int end = -1;
-        for (auto point : points) {
-            int x = point[0];
-            if (end == -1 || x > end) {
+        for(auto it:nums){
+            if(end==-1 || it[0]>end){
                 c++;
-                end = x + w; 
+                end = it[0]+w;
             }
         }
         return c;
