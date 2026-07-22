b473f866d0c5a7b71539f2157bbce1b57bec0338
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-22 20:42:51 +0530

    Time: 0 ms (100%), Space: 13.2 MB (53.71%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,48 @@
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    bool unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return false;         
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DisjointSet ds(n);        
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            if (!ds.unionByRank(u, v)) {
+                return edge; 
+            }
+        }
+        
+        return {};
+    }
+};
