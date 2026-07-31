d6d9ed95cdeff64e26426179e073d148fabf7867
Tadiparthi Abhinav <tadiparthivenkataabhinav@gmail.com>
2026-07-31 15:53:58 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,18 +14,18 @@ public:
         vector< vector<int>> dp(m,vector<int>(n,-1));
         dp[0][0] = 1;
         
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int down = 0;
-                int right = 0;
-                if(i==0 && j==0) continue;
-                if(i-1>=0)  down = dp[i-1][j];
-                if(j-1>=0) right = dp[i][j-1];
-                dp[i][j] = down + right;
-            }
+        // for(int i =0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         int down = 0;
+        //         int right = 0;
+        //         if(i==0 && j==0) continue;
+        //         if(i-1>=0)  down = dp[i-1][j];
+        //         if(j-1>=0) right = dp[i][j-1];
+        //         dp[i][j] = down + right;
+        //     }
             
-        }
-        //return paths(m-1,n-1,dp);
-        return dp[m-1][n-1];
+        // }
+        return paths(m-1,n-1,dp);
+        // return dp[m-1][n-1];
     }
 };
